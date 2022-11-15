3948aa1d10a768256a9d02022633bd4ed43153f0
Jagar <65661908+mongoloidist@users.noreply.github.com>
2022-11-15 10:50:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>for github only</h1>
-      <h2>this is me</h2>
+      <h1>React js project</h1>
+      <h2>This is react js project uploaded into github</h2>
       <img src="https://www.4webs.es/blog/wp-content/uploads/2019/02/urls-que-es-800x420.jpg" alt="someImage" />
     </div>
   );
